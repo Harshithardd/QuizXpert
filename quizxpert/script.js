c9b81c5a572aa131d07e38e25dea8a6681e12b83
Harshithardd <79263383+Harshithardd@users.noreply.github.com>
2023-08-15 22:02:51 +0530

    Add files via upload

---
+++ quizxpert/script.js
@@ -0,0 +1,162 @@
+const questionContainer = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+const restartButton = document.getElementById("restart-btn");
+const resultDiv = document.getElementById("result");
+
+let shuffledQuestions, currentQuestionIndex, score;
+
+const questions = [
+  {
+    question: "What is 2 + 2?",
+    answers: [
+      { text: "4", correct: true },
+      { text: "22", correct: false },
+      { text: "222", correct: false },
+      { text: "2222", correct: false },
+    ],
+  },
+  {
+    question: "What does HTML stand for?",
+    answers: [
+      { text: "HyperText Markup Language", correct: true },
+      { text: "HighText Machine Language", correct: false },
+      { text: "HyperText Machine Language", correct: false },
+      { text: "HighText Markup Language", correct: false },
+    ],
+  },
+  {
+    question: "Which property is used to change the background color?",
+    answers: [
+      { text: "color", correct: false },
+      { text: "bgColor", correct: false },
+      { text: "background-color", correct: true },
+      { text: "background", correct: false },
+    ],
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+      { text: "<js>", correct: false },
+      { text: "<javascript>", correct: false },
+      { text: "<script>", correct: true },
+      { text: "<scripting>", correct: false },
+    ],
+  },
+
+  {
+    question: "How do you make a text bold in CSS?",
+    answers: [
+      { text: "font-weight: bold;", correct: true },
+      { text: "text-style:bold", correct: false },
+      { text: "font-style:bold", correct: false },
+      { text: "font-bold: true;", correct: false },
+    ],
+  },
+
+  {
+    question: "Which property is used to create rounded corners for an element?",
+    answers: [
+      { text: "round-border", correct: false },
+      { text: "rounded-border", correct: false },
+      { text: "border-radius", correct: true },
+      { text: "corner-border", correct: false },
+    ],
+  },
+  {
+    question: "How do you select an element with the class - example in CSS?",
+    
+    answers: [
+      { text: "#example", correct: false },
+      { text: ".example", correct: false },
+      { text: "element.example", correct: false },
+      { text: "class.example", correct: true },
+    ],
+  },
+  {
+    question: "Which CSS property is used to change the font of an element?",
+    answers: [
+      { text: "font-family", correct: true },
+      { text: "text-family", correct: false },
+      { text: "text-font", correct: false },
+      { text: "font-style", correct: false },
+    ],
+  },
+
+];
+
+startQuiz();
+
+function startQuiz() {
+  score = 0;
+  questionContainer.style.display = "flex";
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  nextButton.classList.remove("hide");
+  restartButton.classList.add("hide");
+  resultDiv.classList.add("hide");
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer, index) => {
+    const inputGroup = document.createElement("div");
+    inputGroup.classList.add("input-group");
+
+    const radio = document.createElement("input");
+    radio.type = "radio";
+    radio.id = "answer" + index;
+    radio.name = "answer";
+    radio.value = index;
+
+    const label = document.createElement("label");
+    label.htmlFor = "answer" + index;
+    label.innerText = answer.text;
+
+    inputGroup.appendChild(radio);
+    inputGroup.appendChild(label);
+    answerButtons.appendChild(inputGroup);
+  });
+}
+
+function resetState() {
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+nextButton.addEventListener("click", () => {
+  const answerIndex = Array.from(
+    answerButtons.querySelectorAll("input")
+  ).findIndex((radio) => radio.checked);
+  if (answerIndex !== -1) {
+    if (shuffledQuestions[currentQuestionIndex].answers[answerIndex].correct) {
+      score++;
+    }
+    currentQuestionIndex++;
+    if (shuffledQuestions.length > currentQuestionIndex) {
+      setNextQuestion();
+    } else {
+      endQuiz();
+    }
+  } else {
+    alert("Please select an answer.");
+  }
+});
+
+restartButton.addEventListener("click", startQuiz);
+
+function endQuiz() {
+  questionContainer.style.display = "none";
+  nextButton.classList.add("hide");
+  restartButton.classList.remove("hide");
+  resultDiv.classList.remove("hide");
+  resultDiv.innerText = `Your final score: ${score} / ${shuffledQuestions.length}`;
+}
